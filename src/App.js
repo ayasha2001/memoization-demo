d5b8af475b54bdc939d775ec00d3051f45c3bd7b
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2023-12-29 10:46:27 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from "react";
+import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [listTitle, setListTitle] = useState("My List");
+  const [ascending, setAscending] = useState(true);
+  const [btnText, setBtnText] = useState("change to descending");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const changeOrder = () => {
+    setAscending((prevAscending) => !prevAscending);
+  };
+
+  useEffect(() => {
+    setBtnText(ascending ? "change to descending" : "change to ascending");
+  }, [ascending]);
+
+  const listItems = [5, 3, 1, 10, 9];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <DemoList title={listTitle} items={listItems} ascending={ascending} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <Button onClick={changeOrder}>{btnText}</Button>
     </div>
   );
 }
